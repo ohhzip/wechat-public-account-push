9e15b57006b51420586e64d84f3305e34a03318b
ohhzip <113592561+ohhzip@users.noreply.github.com>
2022-09-16 17:09:18 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wxc90fcbd0b20dd3e3",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
